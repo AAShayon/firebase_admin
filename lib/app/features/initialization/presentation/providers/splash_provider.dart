7ba247adb9b25b3aa9d053eb8e79cb6f055743ac
MD Asif Afroj Shayon <shayon.asif@gmail.com>
2025-05-24 15:00:39 +0600

    refactor: Add debug log for current user in SplashProvider
    This commit adds a debug print statement to the `splashProvider` to log the display name of the current Firebase user during app initialization. This can be helpful for debugging authentication state.

--- lib/app/features/initialization/presentation/providers/splash_provider.dart
+++ lib/app/features/initialization/presentation/providers/splash_provider.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../core/network/firebase_provider.dart';
@@ -6,5 +7,6 @@ import '../../../../core/network/firebase_provider.dart';
 final splashProvider = FutureProvider<User?>((ref) async {
   await Future.delayed(const Duration(seconds: 3));
   final user = FirebaseProvider.auth.currentUser;
+  debugPrint('SplashProvider - Current user: ${user?.displayName}');
   return user;
 });
